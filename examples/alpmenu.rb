109098b9fb68d7fd962b65decfee6a22eff46e98
ChrisArcand <chris@chrisarcand.com>
2014-09-28 21:56:59 -0400

    Fix alpmenu pathing

--- examples/alpmenu.rb
+++ examples/alpmenu.rb
@@ -1,33 +1,37 @@
 require 'rbhex/core/util/app'
 
+  def load_relative(path)
+    load File.expand_path("../#{path}", __FILE__)
+  end
+
   App.new do
     #title "Demo of Menu - rbhex"
     #subtitle "Hit F1 to quit, F2 for menubar toggle"
-    header = app_header "rbhex #{rbhex::VERSION}", :text_center => "Alpine Menu Demo", :text_right =>""
+    header = app_header "rbhex", :text_center => "Alpine Menu Demo", :text_right =>""
 
     stack :margin_top => 10, :margin_left => 15 do
       #w = "Messages".length + 1
       w = 60
       menulink :text => "&View Dirs", :width => w, :description => "View Dirs in tree"  do |s, *stuff|
         message "Pressed #{s.text} "
-        load './dirtree.rb'
+        load_relative './dirtree.rb'
         #require './viewtodo'; todo = ViewTodo::TodoApp.new; todo.run
       end
       blank
       menulink :text => "&Tabular", :width => w, :description => "Tabula Rasa"  do |s, *stuff|
         message "Pressed #{s.text} "
-        load './tabular.rb'
+        load_relative './tabular.rb'
         #require './testtodo'; todo = TestTodo::TodoApp.new; todo.run
       end
       blank
       menulink :text => "&Messages", :width => w, :description => "View Tasks"  do |s, *stuff|
         message "Pressed #{s.text} "
-        load './tasks.rb'
+        load_relative './tasks.rb'
       end
       blank
       menulink :text => "&Database", :width => w, :description => "Database Demo"  do |s, *stuff|
         message "Pressed #{s.getvalue} "
-        load './dbdemo.rb'
+        load_relative './dbdemo.rb'
       end
       blank
       # somehow ? in mnemonic won't work
